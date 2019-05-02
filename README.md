b761dd5b1d87e46f931b409ff21d4d3a92861965
Bowser65 <contact.bowser65@gmail.com>
2019-05-02 22:48:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Urban
+
+Allows you to search on urban dictionary and send the result, locally or in the chat
---
+++ index.js
@@ -0,0 +1,34 @@
+const { Plugin } = require('powercord/entities');
+const { get } = require('powercord/http');
+
+module.exports = class Urban extends Plugin {
+  startPlugin () {
+    this.registerCommand(
+      'urban',
+      [],
+      'Searches on urban dictionary and send the result, locally or in the chat',
+      '{c} <your search> [--send]',
+      this.urban.bind(this)
+    );
+  }
+
+  async urban (args) {
+    let send = false;
+    if (args[args.length - 1] === '--send') {
+      args.pop();
+      send = true;
+    }
+    const data = await get(`https://api.urbandictionary.com/v0/define?term=${encodeURI(args.join(' '))}`).then(r => r.body);
+    if (!data.list || data.list.length === 0) {
+      return {
+        send: false,
+        result: 'Something broke or your query returned nothing'
+      };
+    }
+
+    return {
+      send,
+      result: `Urban definition for \`${args.join(' ')}\`:\n\`\`\`\n${data.list[0].definition}\n\`\`\`\nLink: <${data.list[0].permalink}>`
+    };
+  }
+};
